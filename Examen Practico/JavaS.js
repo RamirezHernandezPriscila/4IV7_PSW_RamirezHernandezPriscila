10cdc0c6d16ed92b14f2c61c9c29d92b700a96f5
RamirezHernandezPriscila <ramirez.hernandez.prisciila2105@gmail,com>
2022-03-03 23:43:47 -0600

    Priscila Ramirez Hernandez

---
+++ Examen Practico/JavaS.js
@@ -0,0 +1,10 @@
+document.getElementById("agrgar").onclick = fuction () {
+    var id_valor=document.getId("valor").value;
+    var id_cuota=document.getIdId("cuota").value;
+    var id_periodo=document.getId("periodo").value;
+
+var table_node = document.getName("table")[0];
+
+
+}
+
